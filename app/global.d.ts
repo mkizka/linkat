0048ef5dfaad049cf72bccb02b724027a2a68593
mkizka <g.mkizka@gmail.com>
2024-08-06 19:09:18 +0900

    ビルド前に型チェック

---
+++ app/global.d.ts
@@ -0,0 +1,11 @@
+// generated以下に登場するが今のところ必要ない型なので無視する
+declare module "multiformats/cid" {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  export const CID = any;
+}
+
+interface Window {
+  bluesky?: {
+    scan?: () => void;
+  };
+}
