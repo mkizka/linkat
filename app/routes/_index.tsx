f243c473f3843baf6df22e3268910fa0250698fd
mkizka <g.mkizka@gmail.com>
2024-08-09 23:56:10 +0900

    ログイン済みならリダイレクトする

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,17 @@
-import type { ClientLoaderFunction } from "@remix-run/react";
+import { type ClientLoaderFunction, redirect } from "@remix-run/react";
 import { createStore } from "jotai";
 
+import { userAtom } from "~/atoms/user/base";
 import { resumeSessionAtom } from "~/atoms/user/write-only";
 import { LoginForm } from "~/components/login-form";
 
 export const clientLoader: ClientLoaderFunction = async () => {
   const store = createStore();
   await store.set(resumeSessionAtom);
+  const user = store.get(userAtom);
+  if (user) {
+    return redirect("/edit");
+  }
   return null;
 };
 
