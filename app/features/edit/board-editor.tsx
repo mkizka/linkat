5fbe04f5bb52272542cd49a7bd30a7ddd6e865d7
mkizka <g.mkizka@gmail.com>
2024-08-14 00:44:52 +0900

    PDSに保存する処理実装

--- app/features/edit/board-editor.tsx
+++ app/features/edit/board-editor.tsx
@@ -4,8 +4,16 @@ import { useState } from "react";
 import { Button } from "~/components/button";
 
 import { Sortable } from "./sortable";
+import { useUpdateBoard } from "./use-submit-board";
 
-const sampleCards = [
+// TODO: Card型を整理する
+type Card = {
+  id: string;
+  text: string;
+  url?: string;
+};
+
+const sampleCards: Card[] = [
   {
     id: "1",
     text: "1. URLあり",
@@ -24,10 +32,15 @@ const sampleCards = [
 
 export function BoardEditor() {
   const [cards, setCards] = useState(sampleCards);
+  const updateBoard = useUpdateBoard();
+
   return (
     <div className="py-8">
       <Sortable cards={cards} setCards={setCards} />
-      <Button className="btn-circle btn-lg fixed bottom-4 right-4 w-32 shadow">
+      <Button
+        className="btn-circle btn-lg fixed bottom-4 right-4 w-32 shadow"
+        onClick={() => updateBoard(cards)}
+      >
         <PencilSquareIcon className="size-8" />
         保存
       </Button>
--- app/models/board.ts
+++ app/models/board.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import { cardSchema } from "./card";
 
 export const boardScheme = z.object({
-  id: z.literal("self"),
   description: z.string().optional(),
   cards: z.array(cardSchema),
 });
