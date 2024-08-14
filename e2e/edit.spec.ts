8dcd6578a41f0107c3b6338b20ac5580c8c0288f
mkizka <g.mkizka@gmail.com>
2024-08-14 23:28:04 +0900

    セッションが期限切れのテスト追加

--- e2e/edit.spec.ts
+++ e2e/edit.spec.ts
@@ -1,7 +1,30 @@
 import { test } from "@playwright/test";
 
+const DUMMY_EXPIRED_USER = JSON.stringify({
+  profile: {},
+  session: {
+    accessJwt: "expired",
+    refreshJwt: "expired",
+    handle: "alice.test",
+    did: "did:plc:prh5i45qtb3sck6suspbccon",
+    email: "alice@test.com",
+    emailConfirmed: false,
+    active: true,
+  },
+  service: "http://localhost:2583/",
+});
+
 test.describe("編集", () => {
   test("非ログイン時はトップにリダイレクト", async ({ page }) => {
+    await page.goto("/edit");
+    await page.waitForURL((url) => url.pathname === "/");
+    await page.waitForTimeout(2000);
+  });
+  test("ログインが無効な時はトップにリダイレクト", async ({ page }) => {
+    await page.goto("/");
+    await page.evaluate((user) => {
+      localStorage.setItem("user", user);
+    }, DUMMY_EXPIRED_USER);
     await page.goto("/edit");
     await page.waitForURL((url) => url.pathname === "/");
     await page.waitForTimeout(2000);
