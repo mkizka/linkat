7c3920ed0280afa0f168393bd8198dc723306331
mkizka <g.mkizka@gmail.com>
2024-08-18 01:13:50 +0900

    eslintルール追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
 import { configs } from "@mkizka/eslint-config";
+import { arrangeActAssert } from "@mkizka/eslint-plugin-aaa";
 
 export default [
   ...configs.typescript(),
   ...configs.react(),
   ...configs.tailwind(),
+  {
+    files: ["**/*.spec.ts"],
+    ignores: ["e2e/**/*.spec.ts"],
+    ...arrangeActAssert,
+  },
 ];
