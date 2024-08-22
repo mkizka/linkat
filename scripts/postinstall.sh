60f07237dd3fdb0d75d19f67dc10456070d73378
mkizka <g.mkizka@gmail.com>
2024-08-22 19:48:13 +0900

    atprotoディレクトリをキャッシュ

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ATPROTO_COMMIT=f2f8de63b333448d87c364578e023ddbb63b8b25
+
 # git submoduleを使うとRailwayで動作しないため、gigetを使ってatprotoを取得する
 if [ ! -d atproto ]; then
   # https://github.com/bluesky-social/atproto/commit/f2f8de63b333448d87c364578e023ddbb63b8b25
-  pnpm giget gh:bluesky-social/atproto#f2f8de63b333448d87c364578e023ddbb63b8b25 atproto
+  pnpm giget gh:bluesky-social/atproto#$ATPROTO_COMMIT atproto
 fi
 mkdir -p ./lexicons/com/atproto/repo
 cp -r ./atproto/lexicons/com/atproto/repo ./lexicons/com/atproto/repo
