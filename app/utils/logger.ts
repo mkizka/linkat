018515cdf97272ff2c548fa32e8a9371fa50ef5f
mkizka <g.mkizka@gmail.com>
2024-08-24 17:33:39 +0900

    LOG_LEVELに対応

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,41 +1,56 @@
 /* eslint-disable no-console */
+import { env } from "./env";
+
+type LogLevel = typeof env.LOG_LEVEL;
 
 // https://docs.railway.app/guides/logs#structured-logs
 type StructuredLog = {
   message: string;
-  level: "debug" | "info" | "warn" | "error";
+  level: LogLevel;
+  name: string;
   [key: string]: string | number | boolean | object | null;
 };
 
-// const logLevelsOrder = {
-//   debug: 0,
-//   info: 1,
-//   warn: 2,
-//   error: 3,
-// } satisfies Record<LogLevel, number>;
+const logLevelsOrder = {
+  debug: 0,
+  info: 1,
+  warn: 2,
+  error: 3,
+} satisfies Record<LogLevel, number>;
 
-// const isLower = (a: LogLevel, b: LogLevel) => {
-//   return logLevelsOrder[a] < logLevelsOrder[b];
-// };
+const isLower = (a: LogLevel, b: LogLevel) => {
+  return logLevelsOrder[a] < logLevelsOrder[b];
+};
 
-// const isSameOrHigher = (a: LogLevel, b: LogLevel) => {
-//   return !isLower(a, b);
-// };
+const writeLogBrowser = (log: StructuredLog) => {
+  if (process.env.NODE_ENV === "production") {
+    return;
+  }
+  const { message, level, name, ...data } = log;
+  const text = `[${name}] ${message}`;
+  if (Object.keys(data).length > 0) {
+    console[level](text, data);
+  } else {
+    console[level](text);
+  }
+};
 
-const writeLog = (log: StructuredLog & { name: string }) => {
+const writeLogServer = (log: StructuredLog) => {
+  // クライアント側でこのファイルをimportするとエラーになってしまうため、動的にimportする
+  if (process.env.NODE_ENV === "test" || isLower(log.level, env.LOG_LEVEL)) {
+    return;
+  }
+  console[log.level](JSON.stringify(log));
+};
+
+const writeLog = (log: StructuredLog) => {
   if (process.env.NODE_ENV === "test") {
     return;
   }
   if (typeof window !== "undefined") {
-    const { message, level, name, ...data } = log;
-    const text = `[${name}] ${message}`;
-    if (Object.keys(data).length > 0) {
-      console[level](text, data);
-    } else {
-      console[level](text);
-    }
+    writeLogBrowser(log);
   } else {
-    console[log.level](JSON.stringify(log));
+    writeLogServer(log);
   }
 };
 
