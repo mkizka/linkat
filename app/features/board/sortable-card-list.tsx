e4fce0c411d7062d94f48e31764c079d755bf245
mkizka <g.mkizka@gmail.com>
2024-08-25 18:27:46 +0900

    useHydrated削除

--- app/features/board/sortable-card-list.tsx
+++ app/features/board/sortable-card-list.tsx
@@ -1,7 +1,6 @@
 import { arrayMoveImmutable } from "array-move";
 import type { Dispatch, SetStateAction } from "react";
 import SortableList, { SortableItem } from "react-easy-sort";
-import { useHydrated } from "remix-utils/use-hydrated";
 
 import type { SortableCardProps } from "./sortable-card";
 import { SortableCard } from "./sortable-card";
@@ -17,21 +16,9 @@ type Props = {
 };
 
 export function SortableCardList({ cards, setCards, sortable }: Props) {
-  const hydrated = useHydrated();
-
   const onSortEnd = (oldIndex: number, newIndex: number) => {
     setCards((cards) => arrayMoveImmutable(cards, oldIndex, newIndex));
   };
-
-  if (!hydrated) {
-    return (
-      <div className={containerClass}>
-        {cards.map((card) => (
-          <SortableCard key={card.id} card={card} />
-        ))}
-      </div>
-    );
-  }
 
   return (
     <SortableList
