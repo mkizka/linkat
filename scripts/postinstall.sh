9138aa496a4452b9f277f151930120d29a90a42d
mkizka <g.mkizka@gmail.com>
2024-08-27 01:57:42 +0900

    fix

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,8 +8,8 @@ if [ ! -d atproto ]; then
   # https://github.com/bluesky-social/atproto/commit/f2f8de63b333448d87c364578e023ddbb63b8b25
   pnpm giget gh:bluesky-social/atproto#$ATPROTO_COMMIT atproto
 fi
-mkdir -p ./lexicons/com/atproto/repo
-cp -r ./atproto/lexicons/com/atproto/repo ./lexicons/com/atproto/repo
+mkdir -p ./lexicons/com/atproto
+cp -r ./atproto/lexicons/com/atproto/repo ./lexicons/com/atproto
 
 LEXICONS=$(find ./lexicons -name '*.json' -type f)
 echo y | pnpm lex gen-api ./app/generated/api $LEXICONS
