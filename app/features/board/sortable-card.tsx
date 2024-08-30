e27951216a3a4011517e0be9d2c01bf16d971004
mkizka <g.mkizka@gmail.com>
2024-08-30 21:53:59 +0900

    つかんでソートするように

--- app/features/board/sortable-card.tsx
+++ app/features/board/sortable-card.tsx
@@ -1,4 +1,4 @@
-import { LinkIcon } from "@heroicons/react/24/outline";
+import { ArrowsUpDownIcon, LinkIcon } from "@heroicons/react/24/outline";
 import { forwardRef } from "react";
 
 import { Card } from "~/components/card";
@@ -17,13 +17,20 @@ export const SortableCard = forwardRef<HTMLDivElement, SortableCardProps>(
       <Card
         ref={ref}
         className={cn({
-          "ring ring-primary": isDragging,
+          "ring ring-primary animate-in zoom-in-100 scale-[103%]": isDragging,
         })}
         data-testid={`sortable-card`}
       >
         <div className="card-body flex-row items-center gap-4">
           <LinkIcon className="size-8" />
           <p className="flex-1 truncate">{card.text || card.url}</p>
+          <button
+            className="btn btn-square btn-outline"
+            tabIndex={-1}
+            data-movable-handle
+          >
+            <ArrowsUpDownIcon className="size-6" />
+          </button>
         </div>
       </Card>
     );
