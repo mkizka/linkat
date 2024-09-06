bdf1481416f2e38a552dcc8eae5c4fad0e96146a
mkizka <g.mkizka@gmail.com>
2024-09-06 23:02:22 +0900

    e2eテスト修正

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,7 +1,13 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
-const prod = process.env.NODE_ENV === "production";
+const match = <Prod, Default>({ prod, dev }: { prod: Prod; dev: Default }) => {
+  if (process.env.NODE_ENV === "production" && !process.env.E2E) {
+    return prod;
+  } else {
+    return dev;
+  }
+};
 
 const server = {
   NODE_ENV: z
@@ -9,20 +15,35 @@ const server = {
     .default(process.env.NODE_ENV),
   LOG_LEVEL: z
     .enum(["debug", "info", "warn", "error"])
-    .default(prod ? "info" : "debug"),
+    .default(match({ prod: "info", dev: "debug" })),
   DATABASE_URL: z.string(),
   BSKY_PUBLIC_API_URL: z
     .string()
     .url()
-    .default(prod ? "https://public.api.bsky.app" : "http://localhost:2584"),
+    .default(
+      match({
+        prod: "https://public.api.bsky.app",
+        dev: "http://localhost:2584",
+      }),
+    ),
   BSKY_FIREHOSE_URL: z
     .string()
     .url()
-    .default(prod ? "wss://bsky.network" : "ws://localhost:2583"),
+    .default(
+      match({
+        prod: "wss://bsky.network",
+        dev: "ws://localhost:2583",
+      }),
+    ),
   ATPROTO_PCL_URL: z
     .string()
     .url()
-    .default(prod ? "https://plc.directory" : "http://localhost:2582"),
+    .default(
+      match({
+        prod: "https://plc.directory",
+        dev: "http://localhost:2582",
+      }),
+    ),
 };
 
 export const env = (() => {
