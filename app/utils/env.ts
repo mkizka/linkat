4c7dfa7021a977f5dcc7ec72bd96e65005ae38f2
mkizka <g.mkizka@gmail.com>
2024-09-06 22:42:31 +0900

    環境変数のデフォルト値を実装側に移動

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,17 +1,28 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+const prod = process.env.NODE_ENV === "production";
+
 const server = {
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default(process.env.NODE_ENV),
   LOG_LEVEL: z
     .enum(["debug", "info", "warn", "error"])
-    .default(process.env.NODE_ENV === "production" ? "info" : "debug"),
+    .default(prod ? "info" : "debug"),
   DATABASE_URL: z.string(),
-  BSKY_PUBLIC_API_URL: z.string().url(),
-  BSKY_FIREHOSE_URL: z.string().url(),
-  ATPROTO_PCL_URL: z.string().url(),
+  BSKY_PUBLIC_API_URL: z
+    .string()
+    .url()
+    .default(prod ? "https://public.api.bsky.app" : "http://localhost:2584"),
+  BSKY_FIREHOSE_URL: z
+    .string()
+    .url()
+    .default(prod ? "wss://bsky.network" : "ws://localhost:2583"),
+  ATPROTO_PCL_URL: z
+    .string()
+    .url()
+    .default(prod ? "https://plc.directory" : "http://localhost:2582"),
 };
 
 export const env = (() => {
