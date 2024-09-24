3665a4a3a1ba6ab2559f43810c841d59ac3ccf01
mkizka <g.mkizka@gmail.com>
2024-09-24 23:11:36 +0900

    useLayoutEffectの警告を修正

--- app/features/board/card/bluesky-embed.tsx
+++ app/features/board/card/bluesky-embed.tsx
@@ -1,10 +1,11 @@
 import { useLayoutEffect } from "react";
+import { ClientOnly } from "remix-utils/client-only";
 
 type Props = {
   blueskyUri: string;
 };
 
-export function BlueskyEmbed({ blueskyUri }: Props) {
+function ClientBlueskyEmbed({ blueskyUri }: Props) {
   useLayoutEffect(() => {
     window.bluesky?.scan?.();
   }, [blueskyUri]);
@@ -14,5 +15,22 @@ export function BlueskyEmbed({ blueskyUri }: Props) {
     <div>
       <blockquote data-bluesky-uri={blueskyUri} />
     </div>
+  );
+}
+
+function Fallback() {
+  return (
+    // 縦幅は埋め込まれる投稿が1行の時のだいたいのサイズ
+    <div className="flex h-32 w-full items-center justify-center">
+      <div className="loading loading-spinner size-8" />
+    </div>
+  );
+}
+
+export function BlueskyEmbed({ blueskyUri }: Props) {
+  return (
+    <ClientOnly fallback={<Fallback />}>
+      {() => <ClientBlueskyEmbed blueskyUri={blueskyUri} />}
+    </ClientOnly>
   );
 }
