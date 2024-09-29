7ed74724508a6eceb17692d33845f2b6f37069b7
mkizka <g.mkizka@gmail.com>
2024-09-29 17:08:01 +0900

    アイコン追加

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <Meta />
         <Links />
       </head>
