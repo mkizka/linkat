5d0a00d8a8722b055cf53a3f327bf88265cfa4ac
mkizka <g.mkizka@gmail.com>
2024-10-18 21:01:21 +0900

    emojiを任意に

--- app/models/card.ts
+++ app/models/card.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 export const cardSchema = z.object({
   url: z.string().url().or(z.literal("")).optional(),
   text: z.string().optional(),
-  emoji: z.string().regex(/^\p{Emoji}$/u),
+  emoji: z
+    .string()
+    .regex(/^\p{Emoji}$/u)
+    .optional(),
 });
 
 export type ValidCard = z.infer<typeof cardSchema>;
