6e708d50c542714d4ed04898a40ad6400137f7ee
mkizka <g.mkizka@gmail.com>
2024-10-25 22:58:11 +0900

    fix e2e

--- e2e/edit.spec.ts
+++ e2e/edit.spec.ts
@@ -34,6 +34,7 @@ test.describe("編集", () => {
     // 保存して閲覧ページで順番を確認
     await page.getByTestId("board-viewer__submit").click();
     await page.waitForURL((url) => url.pathname !== "/edit");
+    await page.keyboard.press("Escape");
     await expect(card1).toBeVisible();
     await expect(card2).toBeVisible();
     const allCards = await page.getByTestId("sortable-card").allTextContents();
@@ -48,6 +49,7 @@ test.describe("編集", () => {
     // 保存して閲覧ページで順番を確認
     await page.getByTestId("board-viewer__submit").click();
     await page.waitForURL((url) => url.pathname !== "/edit");
+    await page.keyboard.press("Escape");
     await expect(card1).toBeVisible();
     await expect(card2).toBeVisible();
     const sorted = await page.getByTestId("sortable-card").allTextContents();
@@ -63,6 +65,7 @@ test.describe("編集", () => {
     // 保存して閲覧ページで順番を確認
     await page.getByTestId("board-viewer__submit").click();
     await page.waitForURL((url) => url.pathname !== "/edit");
+    await page.keyboard.press("Escape");
     await expect(card1).not.toBeVisible();
     await expect(card1Edited).toBeVisible();
     await expect(card2).toBeVisible();
@@ -78,6 +81,7 @@ test.describe("編集", () => {
     // 保存して閲覧ページで順番を確認
     await page.getByTestId("board-viewer__submit").click();
     await page.waitForURL((url) => url.pathname !== "/edit");
+    await page.keyboard.press("Escape");
     await expect(card1Edited).not.toBeVisible();
     await expect(card2).not.toBeVisible();
   });
