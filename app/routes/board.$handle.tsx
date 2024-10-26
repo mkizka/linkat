438db0f416b635a74619ed4bf08929309f00b74f
mkizka <g.mkizka@gmail.com>
2024-10-27 00:00:42 +0900

    /boardからリダイレクト

--- app/routes/board.$handle.tsx
+++ app/routes/board.$handle.tsx
@@ -1,3 +1,6 @@
-import Index, { loader, meta } from "./$handle";
-export { loader, meta };
-export default Index;
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+export function loader({ params }: LoaderFunctionArgs) {
+  return redirect(`/${params.handle}`);
+}
