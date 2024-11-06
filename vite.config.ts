2ec5a7fc8e60db8a96d11fbc62810d085ef30b98
mkizka <g.mkizka@gmail.com>
2024-11-07 00:18:18 +0900

    v3_singleFetch

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_singleFetch: true,
       },
     }),
     tsconfigPaths(),
