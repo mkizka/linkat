959ebc4e6a22252f5ced63a121b2f0f2894b1202
Kazuki Matsuda <g.mkizka@gmail.com>
2024-11-06 22:23:15 +0900

    使用できるURLにhttpとhttpsのみ許可 (#13)

--- app/models/board.ts
+++ app/models/board.ts
@@ -3,7 +3,12 @@ import { z } from "zod";
 import { cardSchema } from "./card";
 
 export const boardScheme = z.object({
-  cards: z.array(cardSchema),
+  cards: z
+    .unknown()
+    .array()
+    .transform((val) => {
+      return val.filter((card) => cardSchema.safeParse(card).success);
+    }),
 });
 
 export type ValidBoard = z.infer<typeof boardScheme>;
