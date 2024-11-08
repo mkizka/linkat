f69e259a0c62f5d79f2b59ad8aa86513c85bc4da
mkizka <g.mkizka@gmail.com>
2024-11-08 21:52:44 +0900

    build/clientをR2にアップロードする

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.VITE_CONFIG_BASE ?? "/",
   plugins: [
     remix({
       future: {
