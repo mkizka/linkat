4813acbcd430c0c9465096bccd11972fbadd921f
mkizka <g.mkizka@gmail.com>
2024-11-10 01:54:56 +0900

    i18next使用時にエスケープしない

--- app/i18n/config.ts
+++ app/i18n/config.ts
@@ -1,10 +1,15 @@
+import type { InitOptions } from "i18next";
+
 import en from "./locales/en.json";
 import ja from "./locales/ja.json";
 
-export const i18nConfig = {
+export const i18nConfig: InitOptions = {
   supportedLngs: ["en", "ja"],
   fallbackLng: "en",
   defaultNS: "translation",
+  interpolation: {
+    escapeValue: false,
+  },
   resources: {
     en: { translation: en },
     ja: { translation: ja },
