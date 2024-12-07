28b106204378155f6df597c9c3a23f3f0526a648
mkizka <g.mkizka@gmail.com>
2024-12-07 21:56:51 +0900

    fix e2e

--- e2e/logout.spec.ts
+++ e2e/logout.spec.ts
@@ -4,14 +4,14 @@ test.describe("ログアウト", () => {
   test("ログアウトボタンを押すとダイアログが出てログアウトできる", async ({
     page,
   }) => {
-    await page.goto("/");
+    await page.goto("/settings");
     page.on("dialog", (dialog) => dialog.accept());
     const logoutButton = page.getByTestId("logout-button");
     await logoutButton.click();
     await expect(logoutButton).not.toBeVisible();
   });
   test("ログアウトボタンを押したあとキャンセルできる", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("/settings");
     page.on("dialog", (dialog) => dialog.dismiss());
     const logoutButton = page.getByTestId("logout-button");
     await logoutButton.click();
