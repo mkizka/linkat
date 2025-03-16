4293026ac2664af06c261c21e6b675a20237ceac
Kazuki Matsuda <g.mkizka@gmail.com>
2025-03-16 14:10:48 +0900

    entry.server.tsxをreact-router v7に追従 (#47)

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,15 +1,9 @@
-/* eslint-disable no-console */
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
 import { PassThrough } from "node:stream";
 
 import { createReadableStreamFromReadable } from "@react-router/node";
 import { createInstance } from "i18next";
 import { isbot } from "isbot";
+import type { RenderToPipeableStreamOptions } from "react-dom/server";
 import { renderToPipeableStream } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import type {
@@ -22,60 +16,54 @@ import { isRouteErrorResponse, ServerRouter } from "react-router";
 
 import { i18nConfig } from "./i18n/config";
 import { i18nServer } from "./i18n/i18n";
+import { createLogger } from "./utils/logger";
 
-const ABORT_DELAY = 5_000;
+export const streamTimeout = 5_000;
 
-export default function handleRequest(
+const createI18nInstance = async (
+  request: Request,
+  routerContext: EntryContext,
+) => {
+  const instance = createInstance();
+  const lng = await i18nServer.getLocale(request);
+  const ns = i18nServer.getRouteNamespaces(routerContext);
+  await instance.use(initReactI18next).init({
+    ...i18nConfig,
+    lng,
+    ns,
+  });
+  return instance;
+};
+
+const logger = createLogger("entry.server");
+
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  reactRouterContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-
+  routerContext: EntryContext,
   loadContext: AppLoadContext,
+  // If you have middleware enabled:
+  // loadContext: unstable_RouterContextProvider
 ) {
-  return isbot(request.headers.get("user-agent") || "")
-    ? handleBotRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        reactRouterContext,
-      )
-    : handleBrowserRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        reactRouterContext,
-      );
-}
-
-async function handleBotRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  reactRouterContext: EntryContext,
-) {
-  const instance = createInstance();
-  const lng = await i18nServer.getLocale(request);
-  const ns = i18nServer.getRouteNamespaces(reactRouterContext);
-  await instance.use(initReactI18next).init({
-    ...i18nConfig,
-    lng,
-    ns,
-  });
+  const instance = await createI18nInstance(request, routerContext);
   return new Promise((resolve, reject) => {
     let shellRendered = false;
+    const userAgent = request.headers.get("user-agent");
+
+    // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
+    // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
+    const readyOption: keyof RenderToPipeableStreamOptions =
+      (userAgent && isbot(userAgent)) || routerContext.isSpaMode
+        ? "onAllReady"
+        : "onShellReady";
+
     const { pipe, abort } = renderToPipeableStream(
       <I18nextProvider i18n={instance}>
-        <ServerRouter
-          context={reactRouterContext}
-          url={request.url}
-          abortDelay={ABORT_DELAY}
-        />
+        <ServerRouter context={routerContext} url={request.url} />
       </I18nextProvider>,
       {
-        onAllReady() {
+        [readyOption]() {
           shellRendered = true;
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
@@ -101,74 +89,17 @@ async function handleBotRequest(
           // errors encountered during initial shell rendering since they'll
           // reject and get logged in handleDocumentRequest.
           if (shellRendered) {
-            console.error(error);
+            logger.error("renderToPipeableStreamでエラーが発生しました", {
+              error,
+            });
           }
         },
       },
     );
 
-    setTimeout(abort, ABORT_DELAY);
-  });
-}
-
-async function handleBrowserRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  reactRouterContext: EntryContext,
-) {
-  const instance = createInstance();
-  const lng = await i18nServer.getLocale(request);
-  const ns = i18nServer.getRouteNamespaces(reactRouterContext);
-  await instance.use(initReactI18next).init({
-    ...i18nConfig,
-    lng,
-    ns,
-  });
-  return new Promise((resolve, reject) => {
-    let shellRendered = false;
-    const { pipe, abort } = renderToPipeableStream(
-      <I18nextProvider i18n={instance}>
-        <ServerRouter
-          context={reactRouterContext}
-          url={request.url}
-          abortDelay={ABORT_DELAY}
-        />
-      </I18nextProvider>,
-      {
-        onShellReady() {
-          shellRendered = true;
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
-
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: responseStatusCode,
-            }),
-          );
-
-          pipe(body);
-        },
-        onShellError(error: unknown) {
-          // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
-          reject(error);
-        },
-        onError(error: unknown) {
-          responseStatusCode = 500;
-          // Log streaming rendering errors from inside the shell.  Don't log
-          // errors encountered during initial shell rendering since they'll
-          // reject and get logged in handleDocumentRequest.
-          if (shellRendered) {
-            console.error(error);
-          }
-        },
-      },
-    );
-
-    setTimeout(abort, ABORT_DELAY);
+    // Abort the rendering stream after the `streamTimeout` so it has time to
+    // flush down the rejected boundaries
+    setTimeout(abort, streamTimeout + 1000);
   });
 }
 
@@ -184,5 +115,5 @@ export function handleError(
   ) {
     return;
   }
-  console.error(error);
+  logger.error("サーバーエラーが発生しました", { error });
 }
