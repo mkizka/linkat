fad932ebcb84d3d3a8d24fbf2720810ec4d9a2aa
mkizka <g.mkizka@gmail.com>
2025-06-26 00:45:44 +0900

    fix

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -2,11 +2,13 @@ import { pino } from "pino";
 
 import { env } from "./env";
 
-const isTest = env.NODE_ENV === "test";
+const isTest = process.env.NODE_ENV === "test";
+
+const isServer = typeof window === "undefined";
 
 const logger = pino({
   enabled: !isTest,
-  level: env.LOG_LEVEL,
+  level: isServer ? env.LOG_LEVEL : "debug",
 });
 
 export const createLogger = (name: string) => {
