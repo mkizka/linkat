74642b6825c573372f1bc67e8000a0a082a84d33
mkizka <g.mkizka@gmail.com>
2025-08-17 20:46:38 +0900

    複数コードポイントの絵文字が使えないのを修正

--- app/models/card.ts
+++ app/models/card.ts
@@ -1,4 +1,10 @@
+import emojiRegex from "emoji-regex";
 import { z } from "zod";
+
+const isOnlySingleEmoji = (val: string) => {
+  const emojis = val.match(emojiRegex());
+  return emojis?.length === 1 && val === emojis[0];
+};
 
 export const cardSchema = z.object({
   url: z
@@ -7,11 +13,7 @@ export const cardSchema = z.object({
     .or(z.literal(""))
     .optional(),
   text: z.string().optional(),
-  emoji: z
-    .string()
-    .regex(/^\p{Emoji}$/u)
-    .or(z.literal(""))
-    .optional(),
+  emoji: z.string().refine(isOnlySingleEmoji).or(z.literal("")).optional(),
 });
 
 export type ValidCard = z.infer<typeof cardSchema>;
