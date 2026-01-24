a96698a399f368a3c289e21b03ec89a36635aeb8
mkizka <g.mkizka@gmail.com>
2026-01-24 19:24:46 +0900

    Revert "ログアウト時にOAuthセッションをrevokeするように修正"
    This reverts commit baf777cdf83343a22323e79321dcf569e7cdb2a6.

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,24 +1,11 @@
 import { redirect } from "react-router";
 
-import { oauthClient } from "~/server/oauth/client";
 import { destroySession, getSession } from "~/server/oauth/session";
-import { createLogger } from "~/utils/logger";
 
 import type { Route } from "./+types/logout";
 
-const logger = createLogger("logout");
-
 export const action = async ({ request }: Route.ActionArgs) => {
   const session = await getSession(request);
-
-  if (session.data.did) {
-    try {
-      await oauthClient.revoke(session.data.did);
-    } catch (error) {
-      logger.error(error, "OAuthセッションのrevokeに失敗しました");
-    }
-  }
-
   return redirect("/", {
     headers: {
       "Set-Cookie": await destroySession(session),
